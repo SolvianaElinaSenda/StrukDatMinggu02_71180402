3f95097f5ed518f8208fc699da9dab13a433ead1
SolvianaElinaSenda <71843652+SolvianaElinaSenda@users.noreply.github.com>
2022-09-07 18:59:35 +0700

    minggu ke-2(mulai susah)
    minggu ke-2(mulai susah)

---
+++ minggu2_71180402.py
@@ -0,0 +1,37 @@
+import time
+
+
+# Mencari deret angka dari 6-100
+
+
+# Fungsi Iteratif
+def fungsi_iteratif(n):
+    n = i
+    for i in range(6, 101):
+        print(i)
+
+
+start = time.time()
+hasil = fungsi_iteratif(100)
+end = time.time()
+
+
+# print(n)
+
+
+# # Fungsi Rekursif
+# def fungsi_rekursif(n):
+#     if n < 5:
+#         return 0
+#     else:
+#         return fungsi_rekursif(n+1)
+
+
+#
+
+
+# hasil1 = fungsi_iteratif(100)
+# print("Fungsi Iteratifnya adalah: ", hasil1)
+
+# hasil2 = fungsi_rekursif(100)
+# print("Fungsi Rekursifnya adalah: ", hasil2)
